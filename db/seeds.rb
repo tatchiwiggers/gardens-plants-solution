5a9e79ffd6b431d74f8cc2b2bc8bdcd48e28f20f
tabatha wiggers <tatisabato@hotmail.com>
2022-11-17 06:25:19 -0300

    done

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Garden.create!(name: 'My super garden', banner_url: 'https://images.unsplash.com/photo-1585320806297-9794b3e4eeae')
+
+Garden.create!(name: 'Other Garden', banner_url: 'https://images.unsplash.com/photo-1578302758063-0ef3e048ca89')
